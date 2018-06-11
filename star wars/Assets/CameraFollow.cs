383eb82900ac518ad8f6f1e7013a2d85719ecba8
sumit singh <30618916+kandicharak@users.noreply.github.com>
2018-06-11 17:32:17 +0530

    first day

---
+++ star wars/Assets/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    public float cameraxaxis = 0f;
+    public float camerayaxis = 0f;
+    public float camerazaxis = 0f;
+    
+    void Update()
+    {
+        Vector3 pos = player.transform.position;
+       
+        pos.z += camerazaxis;
+        pos.y += camerayaxis;
+        pos.x += cameraxaxis;
+        
+        transform.position = pos;
+    }
+}
+
